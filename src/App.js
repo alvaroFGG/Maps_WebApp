51bca35a081f81d3fd716791696d474bdc7f8dcb
alvaroFGG <alvarofuentenebrogomez@gmail.com>
2022-09-02 23:28:57 +0200

    first commit, trying leaflet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer,Marker, Popup  } from 'react-leaflet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MapContainer center={[41.290131, -2.320729]} zoom={15} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+        <Marker position={[41.290131, -2.320729]}>
+          <Popup>
+            Centro del pueblo
+          </Popup>
+        </Marker>
+          
+        </MapContainer>
     </div>
   );
 }
